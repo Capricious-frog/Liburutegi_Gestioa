1e97488780191d05f2a9904be20fd2ebed56754f
Julen Ortiz <Julortizw@gmail.com>
2019-03-13 11:49:06 +0100

    Create Mailegu.java

---
+++ src/biblio/Mailegu.java
@@ -0,0 +1,115 @@
+package biblio;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Mailegu {
+    private int erregistroZenbakia;
+    private String bazkideIzena;
+    private Date maileguData, itzultzeData;
+
+
+    /**
+     * Eraikitzailea
+     *
+     * @param erregistroZenbakia Erregistro zenbakia
+     * @param bazkideIzena Bazkide izena
+     * @param maileguData Mailegu data
+     * @param itzultzeData Itzultze data
+     */
+    public Mailegu(int erregistroZenbakia, String bazkideIzena, Date maileguData, Date itzultzeData) {
+
+    }
+
+
+    /**
+     * Maileguen atributuak idazten ditu pantailan, lerro bakarrean.
+     */
+    public void inprimatu() {
+        SimpleDateFormat dF = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println(erregistroZenbakia + ", "
+                + bazkideIzena + " bazkideari mailegatutako alea; "
+                + "mailegu-data: " + dF.format(this.maileguData)
+                + "; itzultze data:" + dF.format(this.itzultzeData));
+    }
+
+
+    /**
+     * Erregistro zenbakia-ren getter-a
+     *
+     * @return Erregistro zenbakia
+     */
+    public int getErregistroZenbakia() {
+        return erregistroZenbakia;
+    }
+
+
+    /**
+     * Erregistro zenbakia-ren setter-a
+     *
+     * @param erregistroZenbakia Erregistro zenbakia
+     */
+    public void setErregistroZenbakia(int erregistroZenbakia) {
+        this.erregistroZenbakia = erregistroZenbakia;
+    }
+
+
+    /**
+     *
+     *
+     * @return
+     */
+    public String getBazkideIzena() {
+        return bazkideIzena;
+    }
+
+
+    /**
+     *
+     *
+     * @param bazkideIzena Bazkide izena
+     */
+    public void setBazkideIzena(String bazkideIzena) {
+        this.bazkideIzena = bazkideIzena;
+    }
+
+
+    /**
+     *
+     *
+     * @return Mailegu data
+     */
+    public Date getMaileguData() {
+        return maileguData;
+    }
+
+
+    /**
+     *
+     *
+     * @param maileguData Mailegu data
+     */
+    public void setMaileguData(Date maileguData) {
+        this.maileguData = maileguData;
+    }
+
+
+    /**
+     *
+     *
+     * @return Itzultze data
+     */
+    public Date getItzultzeData() {
+        return itzultzeData;
+    }
+
+
+    /**
+     *
+     *
+     * @param itzultzeData Itzultze data
+     */
+    public void setItzultzeData(Date itzultzeData) {
+        this.itzultzeData = itzultzeData;
+    }
+}
