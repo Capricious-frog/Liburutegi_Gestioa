64d0befbcebe14fc5f3fe7fc3b6e799f49541bab
Julen Ortiz <julortizw@gmail.com>
2019-04-17 11:30:14 +0200

    Proba programa

---
+++ src/biblio/ProbatuObraHierarkia.java
@@ -0,0 +1,33 @@
+package biblio;
+
+import java.util.ArrayList;
+import obrak.*;
+public class ProbatuObraHierarkia {
+
+	public static void main(String[] args) {
+				
+		String strLiburu = "1 PTx-001 Printze_Txikia Antoine_de_Saint-Exupery Txertoa Ipuina true";
+		String strAldizkari="10 DON2019-001 Donostia_2019 1 981-UU-003";
+		String strEntziklopedia="7 HARL-001 Harluxet Elhuyar Elhuyar_arg. 12 Euskara Orokorra";
+		String strHiztegi="6 EH-002 Elhuyar_Hiztegia Elhuyar Elhuyar_arg. 1 Euskara-Gaztelania";
+		String strMusikaCD="8 CD-098-ORD Bakarka Elkar Ruper_Ordorika true";
+		String strDVD="3 ZIN-1-CAM Campeones Sony_Pictures_Entertainment Javier_Fesser false";
+		
+		//string zuriunetik banatu eta arrLiburu arrayan sartu
+		String[] arrLiburu = strLiburu.split(" ");
+		//Liburu bat eraiki 
+		
+
+		//Liburuaren metodoak probatu: 
+		//     toString  
+		//     ezaugarrienLista (honek bueltatutako arrayList-eko balioak pantailan idatzi den ondo joan dela ikusteko)
+		
+
+
+
+		//Prozesua errepikatu emandako string bakoitzarentzat
+		
+
+	}
+
+}
